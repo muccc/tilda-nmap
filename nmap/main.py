26da727cfff939a2c31fc16e9aba8b9d32e89f0e
lilafisch <lilafisch@muc.ccc.de>
2016-08-06 13:09:28 +0200

    get human readable bssids

--- nmap/main.py
+++ nmap/main.py
@@ -13,6 +13,12 @@ import time
 import network
 import wifi
 
+import apps.nmap.locations as loc
+import apps.nmap.bssids as bs
+
+
+
+
 ugfx.init()
 buttons.init()
 buttons.disable_menu_reset()
@@ -20,7 +26,16 @@ buttons.disable_menu_reset()
 showMap = 1
 while showMap:
     apList = wifi.nic().list_aps()
-    ugfx.text(10,10,apList,ugfx.BLUE)
+    for i in apList:
+        # get the human readable version of the bssid
+        bssid = ''
+        for idx in range(5):
+	    bssid += str(hex(i['bssid'][idx])[2:4])
+            bssid += ':'
+        bssid += str(hex(i['bssid'][5])[2:4])
+        # plot circle on map
+        
+    ugfx.text(10,10,bssid,ugfx.BLUE)
     time.sleep(10)
 
 
