6514fb695aa9ede0e45b15311d3d57cb8d81b939
lilafisch <lilafisch@muc.ccc.de>
2016-08-06 12:23:20 +0200

    get ap list

--- nmap/main.py
+++ nmap/main.py
@@ -10,15 +10,18 @@ import math
 import ugfx
 import buttons
 import time
+import network
+import wifi
 
 ugfx.init()
 buttons.init()
 buttons.disable_menu_reset()
 
-
 showMap = 1
 while showMap:
-    ugfx.text(10,10,"test",ugfx.BLUE)
+    apList = wifi.nic().list_aps()
+    ugfx.text(10,10,apList,ugfx.BLUE)
     time.sleep(10)
+
 
 
